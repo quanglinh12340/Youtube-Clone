b98e9778be284c1b20f430c7a5e2c0f2d099db57
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-06-12 20:11:26 +0700

    link component PlayVideo

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,10 +1,15 @@
 import classNames from "classnames/bind";
 import styles from "./Video.module.scss";
+import PlayVideo from "@/components/PlayVideo";
 
 const cx = classNames.bind(styles);
 
 const Video = () => {
-  return <div className={cx("play-container")}>Video</div>;
+  return (
+    <div className={cx("play-container")}>
+      <PlayVideo />
+    </div>
+  );
 };
 
 export default Video;
