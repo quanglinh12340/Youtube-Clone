0e415145a98f6b1a34a86ceb9774a9740086b1e2
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-06-12 15:40:23 +0700

    render Ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./Pages/Home";
+import Video from "./Pages/Video";
+import { SidebarProvider } from "./context/Context";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <SidebarProvider>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/video/categoryId/:videoId" element={<Video />} />
+        </Routes>
+      </div>
+    </SidebarProvider>
+  );
 };
 
 export default App;
