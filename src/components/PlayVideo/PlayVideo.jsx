da531ef8f66eb25ceb49f4ff2d43b510180b9403
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-06-12 15:37:45 +0700

    component PlayVideo

---
+++ src/components/PlayVideo/PlayVideo.jsx
@@ -0,0 +1,13 @@
+import classNames from "classnames/bind";
+import styles from "./Navbar.module.scss";
+import { assets } from "@/assets";
+const cx = classNames.bind(styles);
+
+const PlayVideo = () => {
+
+  return (
+   
+  );
+};
+
+export default PlayVideo;
