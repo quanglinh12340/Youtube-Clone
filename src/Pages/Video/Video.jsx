0ac886d75c6c3c7e7dea384875a20e8b255d422e
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-06-13 06:48:56 +0700

    useParams for videoId, categoryId

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,3 +1,4 @@
+import { useParams } from "react-router-dom";
 import classNames from "classnames/bind";
 import styles from "./Video.module.scss";
 import PlayVideo from "@/components/PlayVideo";
@@ -6,9 +7,11 @@ import Recommended from "@/components/Recommended";
 const cx = classNames.bind(styles);
 
 const Video = () => {
+  const { videoId, categoryId } = useParams();
+
   return (
     <div className={cx("play-container")}>
-      <PlayVideo />
+      <PlayVideo videoId={videoId} />
       <Recommended />
     </div>
   );
