2bc2ea41cc4d1bad7871e5d1e72de7f3bff66942
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-06-12 15:35:49 +0700

    component Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import classNames from "classnames/bind";
+import styles from "./Navbar.module.scss";
+import { assets } from "@/assets";
+import { useSidebar } from "@/context/Context";
+const cx = classNames.bind(styles);
+
+const Navbar = () => {
+  const { sidebar, setSidebar } = useSidebar();
+
+  return (
+    <nav className={cx("flex-div")}>
+      <div className={cx("nav-left", "flex-div")}>
+        <img
+          className={cx("menu-icon")}
+          src={assets.menu}
+          onClick={() => (sidebar ? setSidebar(false) : setSidebar(true))}
+        />
+        <img className={cx("logo")} src={assets.logo} />
+      </div>
+      <div className={cx("nav-middle", "flex-div")}>
+        <div className={cx("search-box", "flex-div")}>
+          <input type="text" placeholder="Search" />
+          <img src={assets.search} />
+        </div>
+      </div>
+      <div className={cx("nav-right", "flex-div")}>
+        <img src={assets.upload} />
+        <img src={assets.more} />
+        <img src={assets.notification} />
+        <img src={assets.jack} className={cx("user-icon")} />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
