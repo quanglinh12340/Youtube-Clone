7c3ca3b4c59166c5070c27fb4e91ff86e643f2ba
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-06-13 13:17:43 +0700

    get API and render ui Recommended

--- src/components/Recommended/Recommended.jsx
+++ src/components/Recommended/Recommended.jsx
@@ -1,77 +1,49 @@
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { PropTypes } from "prop-types";
 import classNames from "classnames/bind";
 import styles from "./Recommended.module.scss";
-import { assets } from "@/assets";
+// import { assets } from "@/assets";
+import { API_KEY, valueConverter } from "@/data";
+
 const cx = classNames.bind(styles);
 
-const Recommended = () => {
+const Recommended = ({ categoryId }) => {
+  const [apiData, setApiData] = useState([]);
+
+  const fetchData = async () => {
+    const relatedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=45&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`;
+    await fetch(relatedVideo_url)
+      .then((response) => response.json())
+      .then((data) => setApiData(data.items));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className={cx("recommended")}>
-      <div className={cx("side-video-list")}>
-        <img src={assets.thumbnail1} alt="" />
-        <div className={cx("vid-info")}>
-          <h4>Best channel that help you to be a web developer </h4>
-          <p>GreatStack</p>
-          <p>199K Views</p>
-        </div>
-      </div>
-      <div className={cx("side-video-list")}>
-        <img src={assets.thumbnail2} alt="" />
-        <div className={cx("vid-info")}>
-          <h4>Best channel that help you to be a web developer </h4>
-          <p>GreatStack</p>
-          <p>199K Views</p>
-        </div>
-      </div>
-      <div className={cx("side-video-list")}>
-        <img src={assets.thumbnail3} alt="" />
-        <div className={cx("vid-info")}>
-          <h4>Best channel that help you to be a web developer </h4>
-          <p>GreatStack</p>
-          <p>199K Views</p>
-        </div>
-      </div>
-      <div className={cx("side-video-list")}>
-        <img src={assets.thumbnail4} alt="" />
-        <div className={cx("vid-info")}>
-          <h4>Best channel that help you to be a web developer </h4>
-          <p>GreatStack</p>
-          <p>199K Views</p>
-        </div>
-      </div>
-      <div className={cx("side-video-list")}>
-        <img src={assets.thumbnail5} alt="" />
-        <div className={cx("vid-info")}>
-          <h4>Best channel that help you to be a web developer </h4>
-          <p>GreatStack</p>
-          <p>199K Views</p>
-        </div>
-      </div>
-      <div className={cx("side-video-list")}>
-        <img src={assets.thumbnail6} alt="" />
-        <div className={cx("vid-info")}>
-          <h4>Best channel that help you to be a web developer </h4>
-          <p>GreatStack</p>
-          <p>199K Views</p>
-        </div>
-      </div>
-      <div className={cx("side-video-list")}>
-        <img src={assets.thumbnail7} alt="" />
-        <div className={cx("vid-info")}>
-          <h4>Best channel that help you to be a web developer </h4>
-          <p>GreatStack</p>
-          <p>199K Views</p>
-        </div>
-      </div>
-      <div className={cx("side-video-list")}>
-        <img src={assets.thumbnail8} alt="" />
-        <div className={cx("vid-info")}>
-          <h4>Best channel that help you to be a web developer </h4>
-          <p>GreatStack</p>
-          <p>199K Views</p>
-        </div>
-      </div>
+      {apiData.map((item, index) => {
+        return (
+          <Link
+            to={`/video/${item.snippet.categoryId}/${item.id}`}
+            className={cx("side-video-list")}
+            key={index}
+          >
+            <img src={item.snippet.thumbnails.medium.url} />
+            <div className={cx("vid-info")}>
+              <h4>{item.snippet.title} </h4>
+              <p>{item.snippet.channelTitle}</p>
+              <p>{valueConverter(item.statistics.viewCount)} Views</p>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 };
-
+Recommended.propTypes = {
+  categoryId: PropTypes.node.isRequired,
+};
 export default Recommended;
